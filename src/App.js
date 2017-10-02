26587f2fe4647110f4c89aef299a48f86e4db5ba
lauragreen <lollymay@gmail.com>
2017-10-02 09:50:43 +0100

    Connecting big image to iterated photolist

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
+// import React from 'react';
+// import HelloWorldList from './HelloWorldList';
+// import './App.css';
+//
+// const App = () => {
+//   return (
+//     <div className="App">
+//       <HelloWorldList />
+//     </div>
+//   );
+// };
+//
+// export default App;
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import PhotoList, { BigImage } from './images';
+import BigImage from './images';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Gallery</h2>
-        </div>
         <BigImage />
-        <PhotoList />
       </div>
     );
   }
--- src/images.js
+++ src/images.js
@@ -1,36 +1,59 @@
 import React, { Component } from 'react';
-import data from './image-data.json';
+import photos from './image-data.json';
 import './photos.css';
 
-class PhotoList extends Component {
-  render() {
-    let elem = data.photos;
-    return (
-      <ul className="gallery">
-        {Object.keys(elem).map(i => (
-          <li key={i} className="photoList">
-            <img className="thumb" src={elem[i]['url']} alt={elem[i]['name']} />
-          </li>
-        ))}
-      </ul>
-    );
-  }
-}
+// class PhotoList extends Component {
+//   render() {
+//     let elem = data.photos;
+//     return (
+//       <ul className="gallery">
+//         {Object.keys(elem).map(i => (
+//           <li key={i} className="photoList">
+//             <img className="thumb" src={elem[i].url} alt={elem[i].name} />
+//           </li>
+//         ))}
+//       </ul>
+//     );
+//   }
+// }
 
 class BigImage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { displayedImage: 'hello' };
+    this.selectImage = this.selectImage.bind(this);
+  }
+  selectImage(imageSrc) {
+    console.log(imageSrc);
+    this.setState({ displayedImage: imageSrc });
+  }
+
   render() {
+    let elem = photos.photos;
     return (
-      <div className="bigImageContainer">
-        <img className="bigImage" src="" alt="" />
+      <div>
+        <div className="bigImageContainer">
+          <img className="bigImage" src={this.state.displayedImage} alt="" />
+        </div>
+
+        <ul className="gallery">
+          {Object.keys(elem).map(i => (
+            <li key={i} className="photoList">
+              <img
+                className="thumb"
+                onClick={() => this.selectImage(elem[i].url)}
+                src={elem[i].url}
+                alt={elem[i].name}
+              />
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
 }
 
 export {
-  PhotoList as default,
-  BigImage,
-  // NavDropdown,
-  // NavDropdownTitle,
-  // NavButton
+  // PhotoList as default,
+  BigImage as default,
 };
