3d2670b3f97e40d526bbf64b4009ee1edf3cfa8d
lauragreen <lollymay@gmail.com>
2017-11-13 09:11:26 +0000

    :fire: remove comments

--- src/photo-changer.js
+++ src/photo-changer.js
@@ -1,26 +1,5 @@
 const PhotoChanger = function(state, action) {
-  // 1. ORIGINAL IF STATEMENT
-  // if (action.type === 'next') {
-  //   return { photoIndex: state.photoIndex + 1 };
-  // } else if (action.type === 'previous') {
-  //   return { photoIndex: state.photoIndex - 1 };
-  // }
-
-  // 2. PLAIN SWITCH STATEMENT
-  // switch (action.type) {
-  //   case 'next':
-  //     return {
-  //       photoIndex: state.photoIndex + 1,
-  //     };
-  //     break;
-  //   case 'previous':
-  //     return {
-  //       photoIndex: state.photoIndex - 1,
-  //     };
-  //     break;
-  // }
-
-  // 3. OBJECT LITERAL SWITCH STATEMENT
+  // OBJECT LITERAL SWITCH STATEMENT
   const typeSwitch = type =>
     ({
       next: {
@@ -32,8 +11,5 @@ const PhotoChanger = function(state, action) {
     }[type]);
 
   return typeSwitch(action.type);
-
-  // 1. NEEDED FOR ORIGINAL IF STATEMENT
-  // return state;
 };
 export default PhotoChanger;
